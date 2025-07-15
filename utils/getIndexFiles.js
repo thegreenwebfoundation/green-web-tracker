84b2ad8dd72d4fae805b4f9460b814bde968bfb8
Owen Rogers <exortech@gmail.com>
2025-07-15 10:07:16 -0700

    closes #46, closes #14: specify TRACK_INDEX environment variable to build and test a specific index

--- utils/getIndexFiles.js
+++ utils/getIndexFiles.js
@@ -1,6 +1,7 @@
 import fg from 'fast-glob';
 const dev = process.env.ELEVENTY_RUN_MODE === 'serve';
-
+const trackedIndex = process.env.TRACK_INDEX;
+console.log('getIndexFiles - trackedIndex', trackedIndex)
 export const getIndexFiles = async (dataDir = "_data") => {
     try {
         let filesToCheck = await fg(`src/${dataDir}/indexes/*.json`);
@@ -9,15 +10,17 @@ export const getIndexFiles = async (dataDir = "_data") => {
         const sitesToCheck = filesToCheck.map(async (file) => {
             const data = await import(`../${file}`, { with: { type: 'json' } }).then((data) => {
 
-                if (dev) {
-                    data.default.sites = data.default.sites.slice(0, 10);
-                }
-                
-                return {
+                const index = {
                     filepath: file,
                     filename: file.replace(`src/${dataDir}/indexes/`, '').replace('.json', ''),
                     sites: data.default.sites,
                 };
+
+                if (dev && index.filename !== trackedIndex) {
+                    index.sites = index.sites.slice(0, 10);
+                }
+
+                return index;
             });
 
             return data;
